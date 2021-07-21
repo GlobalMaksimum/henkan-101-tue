c9efa79f5c0bca5d26256ebdf0ce48ba621a4e10
admin <admin@admin.com>
2021-07-21 16:27:59 +0000

    Fix table name

--- sql/f_501_twithtemplate.sql
+++ sql/f_501_twithtemplate.sql
@@ -1,5 +1,5 @@
 #set($loop_end=$date.whenIs('2021-01-01T00:00:00.000').days * -1) /*bir tarihten current_date kadar gitmek isterseniz*/
 
 #{foreach}($i in [1.. $loop_end])
-insert into  temp.velocity_test (id,str,ts,dt) values ($i,$loop_end,sysdate+$i,sysdate+$i);
+insert into  velocity_demo (id,str,ts,dt) values ($i,$loop_end,sysdate+$i,sysdate+$i);
 #{end}
